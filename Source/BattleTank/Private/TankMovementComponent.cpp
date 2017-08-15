fca56d96163540a1743551119aa3ee30db6b7662
sakethkaparthi <sakethkaparthi@outlook.com>
2017-08-16 01:11:49 +0530

    Completed manual movement

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -3,13 +3,20 @@
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
 
-void UTankMovementComponent::IntendMoveForward(float Throw) {
-	LeftTrack->SetThrottle(Throw);
-	RightTrack->SetThrottle(Throw);
-}
-
 void UTankMovementComponent::Initialise(UTankTrack* left, UTankTrack* right) {
 	if (!left || !right) { return; }
 	LeftTrack = left;
 	RightTrack = right;
+}
+
+void UTankMovementComponent::IntendMoveForward(float Throw) {
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw) {
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
--- Source/BattleTank/Public/TankMovementComponent.h
+++ Source/BattleTank/Public/TankMovementComponent.h
@@ -18,6 +18,9 @@ public:
 	UFUNCTION(BlueprintCallable,Category = Input)
 	void IntendMoveForward(float Throw);
 
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw);
+
 	UFUNCTION(BlueprintCallable, Category = Setup)
 	void Initialise(UTankTrack* Left, UTankTrack* Right);
 
